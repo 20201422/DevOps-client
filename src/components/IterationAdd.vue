bc7f91a4ea01c81743f4fa4d46eff49942429d28
LRX <1531373946@qq.com>
2023-05-20 23:39:08 +0800

    改样式

--- src/components/IterationAdd.vue
+++ src/components/IterationAdd.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="manage">
-        <el-button type="primary" @click="dialogVisible = true">创建迭代</el-button>
+        <el-button class="add_button" type="primary" @click="dialogVisible = true">创建迭代</el-button>
         <el-dialog title="创建迭代" v-model="dialogVisible" width="50%">
             <el-form :v-model="form">
                 <label>冲刺名称</label>
@@ -11,14 +11,12 @@
                 <el-form :inline="true" class="demo-form-inline">
                     <el-form-item label="开始时间">
                         <div class="block">
-                            <el-date-picker v-model="beginTime" type="datetime" placeholder="选择开始时间"
-                                :default-time="defaultTime" />
+                            <el-date-picker v-model="form.stratTime" type="datetime" placeholder="选择开始时间" />
                         </div>
                     </el-form-item>
                     <el-form-item label="结束时间">
                         <div class="block">
-                            <el-date-picker v-model="endTime" type="datetime" placeholder="选择结束时间"
-                                :default-time="defaultTime" :disabled-date="disabledDate" />
+                            <el-date-picker v-model="form.endTime" type="datetime" placeholder="选择结束时间"/>
                         </div>
                     </el-form-item>
                 </el-form>
@@ -35,6 +33,7 @@
 <script>
 import {reactive, ref} from 'vue'
 import { ElMessageBox } from "element-plus";
+import Global_color from "@/app/Global_color.vue"
 export default {
     
     name: 'IterationManage',
@@ -44,7 +43,8 @@ export default {
     },
     setup(props) {
         const dialogVisible = ref(false)
-
+        const color1= Global_color.main_color
+        const color2= Global_color.main_color1
         const form = reactive({
             IterationName:"",
             IterationGoal:"",
@@ -54,9 +54,29 @@ export default {
         return {
             dialogVisible,
             form,
+            color1,
+            color2,
         }
     },
+    data(){
+        return {
+            button_color1: Global_color.button_color1,
+            button_color2: Global_color.button_color,
+            write: Global_color.white1,
+        }
+    }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.add_button {
+  background-image: linear-gradient(v-bind(button_color1), v-bind(button_color2));
+  border: solid 1px;
+}
+.add_button:hover {
+  color: v-bind(button_color2);
+  background: v-bind(write);
+  border: solid 1px v-bind(button_color2);
+}
+</style>
--- src/components/IterationMore.vue
+++ src/components/IterationMore.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-button link type="primary" style="margin-left: 16px" @click="this.drawer = true">更多迭代</el-button>
+  <el-text class="button_text" type="primary" style="" @click="this.drawer = true">更多迭代</el-text>
   <el-drawer v-model="drawer" :direction="direction" :close-on-press-escape="true">
     <template #header>
       <h4>迭代计划</h4>
@@ -19,12 +19,12 @@
           </span>
         </el-card>
       </div>
-      
+
     </template>
     <template #footer>
       <div style="flex: auto">
-        <el-button @click="cancelClick">取消</el-button>
-        <el-button type="primary" @click="confirmClick">保存</el-button>
+        <el-button class="btn_1" @click="cancelClick">取消</el-button>
+        <el-button class="btn" type="primary" @click="confirmClick">保存</el-button>
       </div>
     </template>
   </el-drawer>
@@ -33,13 +33,20 @@
 <script>
 import { reactive, ref } from 'vue'
 import { ElMessageBox } from "element-plus";
-
+import Global_color from "@/app/Global_color.vue"
 export default {
   name: "IterationMore",
 
   props: {
     type: String,
     id: String,
+  },
+  data() {
+    return {
+      button_color1:Global_color.button_color1,
+      button_color2: Global_color.button_color,
+      write: Global_color.white1,
+    }
   },
 
   setup(props) {
@@ -115,9 +122,37 @@ export default {
 </script>
   
 <style scoped>
-
 .box-card {
   width: 400px;
+}
+
+.button_text {
+
+  color: v-bind(button_color2);
+
+}
+
+.btn {
+  background-image: linear-gradient(v-bind(button_color1), v-bind(button_color2));
+  border: solid 1px;
+}
+.btn:hover {
+  color: v-bind(button_color2);
+  background: v-bind(write);
+  border: solid 1px v-bind(button_color2);
+}
+.btn_1 {
+  background-image: linear-gradient(v-bind(button_color2), v-bind(button_color1));
+  border: solid 1px;
+}
+.btn_1:hover {
+  color: v-bind(button_color2);
+  background: v-bind(write);
+  border: solid 1px v-bind(button_color2);
+}
+
+.button_text:hover {
+  cursor: pointer;
 }
 </style>
   
